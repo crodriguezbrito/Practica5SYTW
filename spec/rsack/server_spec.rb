11ba6a6a5e48b41c2da8bc5ab25b1bca86d51136
Carlos <crodriguezbrito@gmail.com>
2013-10-22 22:19:53 +0100

    spec

--- spec/rsack/server_spec.rb
+++ spec/rsack/server_spec.rb
@@ -18,4 +18,28 @@ describe Rsack::Server do
 		response.header == ' '
 	end
   end
+  
+  context "/?choice='rock'" do
+
+		it "Debería devolver el código 200" do
+				response = server.get("/?choice='rock'")
+				response.status.should == 200
+		end
+	end
+	
+	context "/?choice='paper'" do
+
+		it "Debería devolver el código 200" do
+				response = server.get("/?choice='paper'")
+				response.status.should == 200
+		end
+	end
+  
+  context "/?choice='scissors'" do
+
+		it "Debería devolver el código 200" do
+				response = server.get("/?choice='scissors'")
+				response.status.should == 200
+		end
+	end
 end
