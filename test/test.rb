cd2cd7483d950b3938089cd095a0bcaa6dc9f65a
Carlos <crodriguezbrito@gmail.com>
2013-10-22 16:01:59 +0100

    añadiendo test

--- test/test.rb
+++ test/test.rb
@@ -17,19 +17,29 @@ class AppTest < Test::Unit::TestCase
 		#puts last_response.inspect
 		assert last_response.ok?
 	end
-  
-   def test_body
-		get "/"
-		assert last_response.ok?
-	end
-   
-
+	
+	def test_header
+		get"/"
+        assert_match "<title>RPS</title>", last_response.body
+    end
+	
 	def test_static_asset
 		get "/public/css/style.css"
 		assert last_response.ok?
 	end
  
-
-
+	 def test_body_rock
+        get "/"
+        assert last_response.body.include?("Rock")
+    end
+    def test_body_paper
+        get "/"
+        assert last_response.body.include?("Paper")
+    end
+    def test_body_scissors
+        get "/"
+        assert last_response.body.include?("Scissors")
+    end
+	
  
 end
